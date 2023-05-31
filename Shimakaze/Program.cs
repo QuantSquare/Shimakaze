568f30906309ea127b3bc0ddd4c7466a40cb3b0f
Kexy Biscuit <xyke@c8-technologies.com>
2023-05-31 17:57:55 +0800

    Partial per file parsing.

--- Shimakaze/Program.cs
+++ Shimakaze/Program.cs
@@ -7,7 +7,10 @@ static Task generateResult(string directory, Provider provider)
 {
     return Task.Run(() =>
     {
-        var result = new StringBuilder();
+        Directory.CreateDirectory($"Results{Path.DirectorySeparatorChar}PositionClosed{Path.DirectorySeparatorChar}{directory.Split(Path.DirectorySeparatorChar)[1]}");
+        Directory.CreateDirectory($"Results{Path.DirectorySeparatorChar}Positions{Path.DirectorySeparatorChar}{directory.Split(Path.DirectorySeparatorChar)[1]}");
+
+        StringBuilder result = new();
 
         switch (provider)
         {
@@ -25,24 +28,24 @@ static Task generateResult(string directory, Provider provider)
                             {
                                 throw new InvalidOperationException();
                             }
-                            _ = result.Append(clientId);
+                            _ = result.Append($"\"{clientId}\"");
                         }
                         if (line.StartsWith("日期："))
                         {
-                            _ = result.Append($",{line.Split(new[] { "日期：" }, StringSplitOptions.RemoveEmptyEntries)[0]}");
+                            _ = result.Append($",\"{line.Split(new[] { "日期：" }, StringSplitOptions.RemoveEmptyEntries)[0]}\"");
                         }
                         if (line.StartsWith("交易日："))
                         {
-                            _ = result.Append($",{line.Split(new[] { "交易日：" }, StringSplitOptions.RemoveEmptyEntries)[0]}");
+                            _ = result.Append($",\"{line.Split(new[] { "交易日：" }, StringSplitOptions.RemoveEmptyEntries)[0]}\"");
                         }
                         if (line.StartsWith("出入金Deposit/Withdrawal："))
                         {
                             var columns = line.Split(new[] { "出入金Deposit/Withdrawal：", "期末结存Balancec/f：" }, StringSplitOptions.RemoveEmptyEntries);
-                            _ = result.Append($",{columns[0]},{columns[1]}");
+                            _ = result.Append($",\"{columns[0]}\",\"{columns[1]}\"");
                         }
                         if (line.StartsWith("权利金收入Premiumreceived："))
                         {
-                            _ = result.Append($",{line.Split(new[] { "权利金收入Premiumreceived：", "风险度RiskDegree：" }, StringSplitOptions.RemoveEmptyEntries)[1]}");
+                            _ = result.Append($",\"{line.Split(new[] { "权利金收入Premiumreceived：", "风险度RiskDegree：" }, StringSplitOptions.RemoveEmptyEntries)[1]}\"");
                         }
                     }
                     _ = result.AppendLine();
@@ -55,6 +58,11 @@ static Task generateResult(string directory, Provider provider)
                 {
                     Console.WriteLine($"Task={Task.CurrentId}, file={file}, Thread={Environment.CurrentManagedThreadId}");
 
+                    string? date = null;
+                    StringBuilder? fileResult = null;
+                    StringBuilder positionClosed = new(), positions = new();
+                    ParseTable? parseTable = null;
+
                     foreach (var line in File.ReadLines(file, Encoding.GetEncoding("GB18030")).Select(s => s.Replace(" ", "")))
                     {
                         if (line.StartsWith("客户号："))
@@ -64,22 +72,53 @@ static Task generateResult(string directory, Provider provider)
                             {
                                 throw new InvalidOperationException();
                             }
-                            _ = result.Append(clientId);
+                            _ = result.Append($"\"{clientId}\"");
                         }
                         if (line.StartsWith("日期："))
                         {
-                            _ = result.Append($",{line.Split(new[] { "日期：" }, StringSplitOptions.RemoveEmptyEntries)[0]}");
+                            date = line.Split(new[] { "日期：" }, StringSplitOptions.RemoveEmptyEntries)[0];
+                            _ = result.Append($",\"{date}\"");
                         }
                         if (line.StartsWith("出入金："))
                         {
                             var columns = line.Split(new[] { "出入金：", "期末结存：", "可用资金：" }, StringSplitOptions.RemoveEmptyEntries);
-                            _ = result.Append($",{columns[0]},{columns[1]}");
+                            _ = result.Append($",\"{columns[0]}\",\"{columns[1]}\"");
                         }
                         if (line.StartsWith("平仓盈亏："))
                         {
-                            _ = result.Append($",{line.Split(new[] { "平仓盈亏：", "质押金：", "风险度：" }, StringSplitOptions.RemoveEmptyEntries)[2]}");
+                            _ = result.Append($",\"{line.Split(new[] { "平仓盈亏：", "质押金：", "风险度：" }, StringSplitOptions.RemoveEmptyEntries)[2]}\"");
+                        }
+                        if (line.StartsWith("|") && !line.StartsWith("|共"))
+                        {
+                            switch (line)
+                            {
+                                case string when line.StartsWith("|成交日期|交易所|品种|交割期|买卖|投保|成交价|手数|成交额|开平|手续费|平仓盈亏|成交序号|成交时间|"):
+                                case string when line.StartsWith("|交割日期|交易所|品种|合约|投/保|买/卖|是否行权|行权数量|行权价格|行权金额|行权盈亏|行权手续费|"):
+                                case string when line.StartsWith("|交易所|合约代码|交割期|开仓日期|投/保|买/卖|持仓量|开仓价|昨结算价|今结算价|浮动盈亏|盯市盈亏|保证金|"):
+                                    fileResult = null;
+                                    parseTable = null;
+                                    break;
+                                case string when line.StartsWith("|平仓日期|交易所|品种|交割期|开仓日期|买/卖|手数|开仓价|昨核算|成交价|平仓盈亏|"):
+                                    fileResult = positionClosed;
+                                    parseTable = ParsePositionClosed;
+                                    break;
+                                case string when line.StartsWith("|合约代码|交割期|买持|买均价|卖持|卖均价|昨结算价|今结算价|持仓盯市盈亏|保证金占用|投/保|"):
+                                    fileResult = positions;
+                                    parseTable = ParsePositions;
+                                    break;
+                                default:
+                                    if (fileResult != null && parseTable != null)
+                                    {
+                                        parseTable.Invoke(line.Split("|"), fileResult, provider);
+                                    }
+                                    break;
+                            }
                         }
                     }
+
+                    File.WriteAllLines($"Results{Path.DirectorySeparatorChar}PositionClosed{Path.DirectorySeparatorChar}{directory.Split(Path.DirectorySeparatorChar)[1]}{Path.DirectorySeparatorChar}PositionClosed_{directory.Split(Path.DirectorySeparatorChar)[1]}_{date}.csv", new[] { positionClosed.ToString() }, Encoding.UTF8);
+                    File.WriteAllLines($"Results{Path.DirectorySeparatorChar}Positions{Path.DirectorySeparatorChar}{directory.Split(Path.DirectorySeparatorChar)[1]}{Path.DirectorySeparatorChar}Positions_{directory.Split(Path.DirectorySeparatorChar)[1]}_{date}.csv", new[] { positions.ToString() }, Encoding.UTF8);
+
                     _ = result.AppendLine();
                 }
                 break;
@@ -97,20 +136,20 @@ static Task generateResult(string directory, Provider provider)
                             {
                                 throw new InvalidOperationException();
                             }
-                            _ = result.Append(clientId);
+                            _ = result.Append($"\"{clientId}\"");
                         }
                         if (line.StartsWith("日期Date："))
                         {
-                            _ = result.Append($",{line.Split(new[] { "日期Date：" }, StringSplitOptions.RemoveEmptyEntries)[0]}");
+                            _ = result.Append($",\"{line.Split(new[] { "日期Date：" }, StringSplitOptions.RemoveEmptyEntries)[0]}\"");
                         }
                         if (line.StartsWith("出入金Deposit/Withdrawal："))
                         {
                             var columns = line.Split(new[] { "出入金Deposit/Withdrawal：", "期末结存Balancec/f：" }, StringSplitOptions.RemoveEmptyEntries);
-                            _ = result.Append($",{columns[0]},{columns[1]}");
+                            _ = result.Append($",\"{columns[0]}\",\"{columns[1]}\"");
                         }
                         if (line.StartsWith("权利金收入Premiumreceived："))
                         {
-                            _ = result.Append($",{line.Split(new[] { "权利金收入Premiumreceived：", "风险度RiskDegree：" }, StringSplitOptions.RemoveEmptyEntries)[1]}");
+                            _ = result.Append($",\"{line.Split(new[] { "权利金收入Premiumreceived：", "风险度RiskDegree：" }, StringSplitOptions.RemoveEmptyEntries)[1]}\"");
                         }
                     }
                     _ = result.AppendLine();
@@ -120,9 +159,49 @@ static Task generateResult(string directory, Provider provider)
                 throw new NotImplementedException();
         }
 
-        File.WriteAllLines($"Result_{directory.Split(Path.DirectorySeparatorChar)[1]}.csv", new[] { result.ToString() }, Encoding.UTF8);
+        File.WriteAllLines($"Results{Path.DirectorySeparatorChar}Result_{directory.Split(Path.DirectorySeparatorChar)[1]}.csv", new[] { result.ToString() }, Encoding.UTF8);
     });
 }
+
+static void ParsePositionClosed(string[] columns, StringBuilder fileResult, Provider provider)
+{
+    switch (provider)
+    {
+        case Provider.Jees:
+            throw new NotImplementedException();
+        case Provider.Lanyee:
+            throw new NotImplementedException();
+        case Provider.Rohon:
+            fileResult.AppendLine($"\"{columns[3]}\",\"{columns[6]}\",\"{columns[7]}\",\"{columns[11]}\"");
+            break;
+        case Provider.Shinny:
+            throw new NotImplementedException();
+        default:
+            throw new NotImplementedException();
+    }
+}
+
+static void ParsePositions(string[] columns, StringBuilder fileResult, Provider provider)
+{
+    switch (provider)
+    {
+        case Provider.Jees:
+            throw new NotImplementedException();
+        case Provider.Lanyee:
+            throw new NotImplementedException();
+        case Provider.Rohon:
+            fileResult.AppendLine($"\"{columns[1]}\",\"{columns[3]}\",\"{columns[5]}\",\"{columns[8]}\",\"{columns[9]}\""); 
+            break;
+        case Provider.Shinny:
+            throw new NotImplementedException();
+        default:
+            throw new NotImplementedException();
+    }
+}
+
+Directory.CreateDirectory("Results");
+Directory.CreateDirectory($"Results{Path.DirectorySeparatorChar}PositionClosed");
+Directory.CreateDirectory($"Results{Path.DirectorySeparatorChar}Positions");
 
 List<Task>? tasks = new();
 
@@ -142,3 +221,13 @@ foreach (var directory in Directory.EnumerateDirectories("Shinny_交易结算单
 }
 
 await Task.WhenAll(tasks);
+
+internal enum Provider
+{
+    Jees,
+    Lanyee,
+    Rohon,
+    Shinny,
+}
+
+internal delegate void ParseTable(string[] columns, StringBuilder fileResult, Provider provider);
--- Shimakaze/Provider.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-internal enum Provider
-{
-    Jees,
-    Lanyee,
-    Rohon,
-    Shinny,
-}
