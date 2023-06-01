c376339cddc143c87dc9abac9e92cd0dcaea8278
Kexy Biscuit <xyke@c8-technologies.com>
2023-06-01 15:41:42 +0800

    Support per file parsing.

--- Shimakaze/Program.cs
+++ Shimakaze/Program.cs
@@ -127,6 +127,11 @@ static Task generateResult(string directory, Provider provider)
                 {
                     Console.WriteLine($"Task={Task.CurrentId}, file={file}, Thread={Environment.CurrentManagedThreadId}");
 
+                    string? date = null;
+                    StringBuilder? fileResult = null;
+                    StringBuilder positionClosed = new(), positions = new();
+                    ParseTable? parseTable = null;
+
                     foreach (var line in File.ReadAllLines(file, Encoding.GetEncoding("GB18030")).Select(s => s.Replace(" ", "")))
                     {
                         if (line.StartsWith("客户号ClientID："))
@@ -140,7 +145,8 @@ static Task generateResult(string directory, Provider provider)
                         }
                         if (line.StartsWith("日期Date："))
                         {
-                            _ = result.Append($",\"{line.Split(new[] { "日期Date：" }, StringSplitOptions.RemoveEmptyEntries)[0]}\"");
+                            date = line.Split(new[] { "日期Date：" }, StringSplitOptions.RemoveEmptyEntries)[0];
+                            _ = result.Append($",\"{date}\"");
                         }
                         if (line.StartsWith("出入金Deposit/Withdrawal："))
                         {
@@ -151,7 +157,42 @@ static Task generateResult(string directory, Provider provider)
                         {
                             _ = result.Append($",\"{line.Split(new[] { "权利金收入Premiumreceived：", "风险度RiskDegree：" }, StringSplitOptions.RemoveEmptyEntries)[1]}\"");
                         }
+                        if (line.StartsWith("|") && !line.StartsWith("|共"))
+                        {
+                            switch (line)
+                            {
+                                case string when line.StartsWith("|发生日期|出入金类型|入金|出金|汇率|资金账号|说明|"):
+                                case string when line.StartsWith("|Date|Type|Deposit|Withdrawal|ExchangeRate|AccountID|Note|"):
+                                case string when line.StartsWith("|成交日期|投资单元|交易所|交易编码|品种|合约|买/卖|投/保|成交价|手数|成交额|开平|手续费|平仓盈亏|权利金收支|成交序号|资金账号|"):
+                                case string when line.StartsWith("|Date|InvestUnit|Exchange|tradingcode|Product|Instrument|B/S|S/H|Price|Lots|Turnover|O/C|Fee|RealizedP/L|PremiumReceived/Paid|Trans.No.|AccountID|"):
+                                case string when line.StartsWith("|平仓日期|投资单元|交易所|交易编码|品种|合约|开仓日期|投/保|买/卖|手数|开仓价|昨结算|成交价|平仓盈亏|权利金收支|资金账号|"):
+                                case string when line.StartsWith("|投资单元|交易所|交易编码|品种|合约|开仓日期|投/保|买/卖|持仓量|开仓价|昨结算|结算价|浮动盈亏|盯市盈亏|保证金|期权市值|资金账号|"):
+                                case string when line.StartsWith("|InvestUnit|Exchange|tradingcode|Product|Instrument|OpenDate|S/H|B/S|Positon|Pos.OpenPrice|Prev.Sttl|SettlementPrice|Accum.P/L|MTMP/L|Margin|MarketValue(Options)|AccountID|"):
+                                case string when line.StartsWith("|投资单元|交易编码|品种|合约|买持|买均价|卖持|卖均价|昨结算|今结算|持仓盯市盈亏|保证金占用|投/保|多头期权市值|空头期权市值|资金账号|"):
+                                    fileResult = null;
+                                    parseTable = null;
+                                    break;
+                                case string when line.StartsWith("|CloseDate|InvestUnit|Exchange|tradingcode|Product|Instrument|OpenDate|S/H|B/S|Lots|Pos.OpenPrice|Prev.Sttl|Trans.Price|RealizedP/L|PremiumReceived/Paid|AccountID|"):
+                                    fileResult = positionClosed;
+                                    parseTable = ParsePositionClosed;
+                                    break;
+                                case string when line.StartsWith("|InvestUnit|tradingcode|Product|Instrument|LongPos.|AvgBuyPrice|ShortPos.|AvgSellPrice|Prev.Sttl|SttlToday|MTMP/L|MarginOccupied|S/H|MarketValue(Long)|MarketValue(Short)|AccountID|"):
+                                    fileResult = positions;
+                                    parseTable = ParsePositions;
+                                    break;
+                                default:
+                                    if (fileResult != null && parseTable != null)
+                                    {
+                                        parseTable.Invoke(line.Split("|"), fileResult, provider);
+                                    }
+                                    break;
+                            }
+                        }
                     }
+
+                    File.WriteAllLines($"Results{Path.DirectorySeparatorChar}PositionClosed{Path.DirectorySeparatorChar}{directory.Split(Path.DirectorySeparatorChar)[1]}{Path.DirectorySeparatorChar}PositionClosed_{directory.Split(Path.DirectorySeparatorChar)[1]}_{date}.csv", new[] { positionClosed.ToString() }, Encoding.UTF8);
+                    File.WriteAllLines($"Results{Path.DirectorySeparatorChar}Positions{Path.DirectorySeparatorChar}{directory.Split(Path.DirectorySeparatorChar)[1]}{Path.DirectorySeparatorChar}Positions_{directory.Split(Path.DirectorySeparatorChar)[1]}_{date}.csv", new[] { positions.ToString() }, Encoding.UTF8);
+
                     _ = result.AppendLine();
                 }
                 break;
@@ -175,7 +216,8 @@ static void ParsePositionClosed(string[] columns, StringBuilder fileResult, Prov
             fileResult.AppendLine($"\"{columns[3]}\",\"{columns[6]}\",\"{columns[7]}\",\"{columns[11]}\"");
             break;
         case Provider.Shinny:
-            throw new NotImplementedException();
+            fileResult.AppendLine($"\"{columns[6]}\",\"{columns[9]}\",\"{columns[10]}\",\"{columns[14]}\"");
+            break;
         default:
             throw new NotImplementedException();
     }
@@ -190,10 +232,11 @@ static void ParsePositions(string[] columns, StringBuilder fileResult, Provider 
         case Provider.Lanyee:
             throw new NotImplementedException();
         case Provider.Rohon:
-            fileResult.AppendLine($"\"{columns[1]}\",\"{columns[3]}\",\"{columns[5]}\",\"{columns[8]}\",\"{columns[9]}\""); 
+            fileResult.AppendLine($"\"{columns[1]}\",\"{columns[3]}\",\"{columns[5]}\",\"{columns[8]}\",\"{columns[9]}\"");
             break;
         case Provider.Shinny:
-            throw new NotImplementedException();
+            fileResult.AppendLine($"\"{columns[4]}\",\"{columns[5]}\",\"{columns[7]}\",\"{columns[10]}\",\"{columns[11]}\"");
+            break;
         default:
             throw new NotImplementedException();
     }
